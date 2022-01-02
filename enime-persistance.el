da62b08231fb0a2ccc9218fce5da1d20f14a7507
xl666 <xavier120@hotmail.com>
2022-01-01 21:42:41 -0600

    Refactoring code to enable a cache

--- enime-persistance.el
+++ enime-persistance.el
@@ -3,19 +3,44 @@
   :group 'enime
   :type 'string)
 
+(defvar enime--followed-anime-alist-cache nil
+  "Holds the currently saved followed anime alist, the idea is to reduce disk access, this variable should be updated every time the contents of the db file change")
+
 (defclass Anime-Storage (eieio-persistent)
   ((data :initarg :menagerie :initform nil)
    (file :initform "")))
 
+(defun enime--get-followed-anime-alist ()
+  "Returns the currently saved followed anime alist"
+  (if enime--followed-anime-alist-cache
+      enime--followed-anime-alist-cache
+    (let* ((store
+	    (condition-case nil
+		(eieio-persistent-read enime-storage-file Anime-Storage)
+	      (error nil))))
+      (when store
+	(setq enime--followed-anime-alist-cache
+	      (oref store data))
+	enime--followed-anime-alist-cache))))
+
+(defun enime--update-db (new-alist)
+  "Updates the db file with new alist, also takes care of cache"
+  (when new-alist
+    (let* ((store
+	    (condition-case nil
+		(eieio-persistent-read enime-storage-file Anime-Storage)
+	      (error nil))))
+      (when (not store)
+	(setf store (Anime-Storage)))
+      (oset store data
+	    new-alist)
+      (eieio-persistent-save store enime-storage-file)
+      (setq enime--followed-anime-alist-cache new-alist))))
+
 (defun enime--follow-anime (anime-id current-episode description)
   "Store new anime to follow"
-  (let* ((store
-	  (condition-case nil
-	      (eieio-persistent-read enime-storage-file Anime-Storage)
-	    (error nil)))
-	 (animes
-	  (when store
-	    (oref store data)))
+  (let* (
+	 (animes (enime--get-followed-anime-alist))
 	 (element `(,anime-id
 		    (:description
 		     ,description
@@ -32,36 +57,21 @@
     (if (and animes (assoc anime-id animes))
 	(message "You're already following this anime")
       (progn
-	(when (not store)
-	  (setf store (Anime-Storage)))
-	(oset store data
-	      (append animes
-		      (list element)))
-	(eieio-persistent-save store enime-storage-file)
+	(enime--update-db (append
+			   animes
+			   (list element)))
 	(message "Following anime")))))
 
 (defun enime--unfollow-anime (anime-id)
   "Unfollow an anime, that is, delete it from db"
-  (let* ((store
-	  (condition-case nil
-	      (eieio-persistent-read enime-storage-file Anime-Storage)
-	    (error nil))))
-    (if store
-	(let ((animes (oref store data)))
-	  (oset store data
-		(assoc-delete-all anime-id animes))
-	  (eieio-persistent-save store enime-storage-file))
-      nil)))
+  (enime--update-db (assoc-delete-all
+		     anime-id
+		     (enime--get-followed-anime-alist))))
+
 
 (defun enime--is-anime-followed-p (anime-id)
   "Predicate to determine if an anime is being followed"
-  (let ((store
-	 (condition-case nil
-	     (eieio-persistent-read enime-storage-file Anime-Storage)
-	   (error nil))))
-    (if store
-	(let ((animes (oref store data)))
-	  (when (assoc anime-id animes)
-	    t))
-      nil)))
+  (when (assoc anime-id
+	       (enime--get-followed-anime-alist))
+    t))
 
